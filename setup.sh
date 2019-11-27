699d40639969d5b45573b3f2429694af4e53ad53
netsensei <matthias.vandermaesen@gmail.com>
2019-11-27 20:58:29 +0100

    Ansible integration.

--- setup.sh
+++ setup.sh
@@ -5,12 +5,17 @@ set -e
 
 REPO=${REPO:-git://github.com/netsensei/workstation.git}
 DIR=${CLONE_DIR:-/home/"${SUDO_USER}"/Workspace/workstation}
+NO_COWSAY=${NO_COWSAY:-1}
 
 # preliminary packages
-dnf install -y vim ansible git
+dnf install -y vim ansible git cowsay
 
 # clone this repo
-mkdir -p "${DIR}"
-git clone --recursive "${REPO}" "${DIR}"
-chown -R ${SUDO_USER}.${SUDO_USER} ${DIR}
 
+if [[ ! -d ${DIR} ]];then
+  mkdir -p "${DIR}"
+  git clone --recursive "${REPO}" "${DIR}"
+  chown -R ${SUDO_USER}.${SUDO_USER} ${DIR}
+fi
+
+ANSIBLE_NOCOWS=${NO_COWSAY} ANSIBLE_COW_SELECTION=tux ansible-playbook -u ${SUDO_USER} -i "${DIR}"/hosts "${DIR}"/playbook.yml
